7da3aec18e705c3915e51ac95967e0df74fbe174
Carmine-55 <saggese04.carmine@gmail.com>
2025-05-09 17:51:51 +0200

    Modifica coda_Priorità.c
    Aggiunta l'implementazione delle funzioni: static void sali(PCoda c), static void scendi (PCoda c)

--- Coda_Priorità.c
+++ Coda_Priorità.c
@@ -6,7 +6,7 @@
 //Definizione della struttura per la coda a priorità
 struct coda_P{
     Attività vet[MAX_HEAP];      //Array di attività memorizzati nell'heap
-    int num_elementi;             //Numero attuale di elementi nell'heap
+    int num_elementi;            //Numero attuale di elementi nell'heap
 };
 
 //Funzione per creare una nuova di coda priorità
@@ -66,3 +66,50 @@ int inserisci(PCoda c, Attività nuovaAttività){
     return 1;                                      //Inserimento riuscito
 }
 
+// Funzione per riaggiustare l'heap verso l'alto
+static void sali(PCoda c){
+    int pos = c->num_elementi, i = pos/2;
+    Attività temp;
+
+    while(pos < 1)
+    {
+     // Se l'elemento inserito ha priorità maggiore del suo genitore, scambia i loro valori
+        if(c->vet[pos].priorità > c->vet[i].priorità){
+        temp = c->vet[i];
+        c->vet[i] = c->vet[pos];
+        c->vet[pos] = temp;
+        pos = i;
+        i = pos / 2;                                //Continua a risalire nell'heap
+        }   else
+              break;                                //Se non sono necessari altri scambi termina
+    }
+}
+
+//Funzione per riaggiustare l'heap verso il basso
+static void scendi(PCoda c){
+    int n = c->num_elementi, pos, i = 1;
+    Attività temp;
+
+    while(1)
+    {
+       // Determina la posizione del figlio con priorità più alta
+
+       if(2*i + 1 <= n)                             //Se il nodo corrente ha due figli
+          pos = (c->vet[2*i].priorità > c->vet[2*i + 1].priorità) ? 2*i : 2*i + 1;
+       else if (2*i <= n)                           //Se il nodo corrente ha un solo figlio
+          pos = 2*i;
+          else
+                    break;                          //Se non ha figli interrompe il ciclo 
+
+      // Scambia i valori se il nodo figlio ha una priorità più alta rispetto al nodo genitore       
+        if (c->vet[pos].priorità > c->vet[i].priorità)
+        {
+             temp = c->vet[i];
+             c->vet[i] = c->vet[pos];
+             c->vet[pos] = temp;
+             i = pos;                               // Continua a scendere nell'heap.
+       }
+       else
+           break;                                   // Termina se non sono necessari altri scambi.
+     }
+}
